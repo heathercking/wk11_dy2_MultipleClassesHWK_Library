e0a45c2f80d46f725b2227cd589473a1e2e410e1
Heather King <hkingdevelopment@gmail.com>
2022-01-05 17:07:25 +0000

    creates Book class and tests and Library class

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("Harry Potter", "JK Rowling", "Fantasy Fiction");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("Harry Potter", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("JK Rowling", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("Fantasy Fiction", book.getGenre());
+    }
+
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.LinkRef;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book("Harry Potter", "JK Rowling", "Fantasy Fiction");
+    }
+
+//    @Test
+//    public void hasBookCollection() {
+//        assertEquals(0, library.countBooks());
+//    }
+
 }
